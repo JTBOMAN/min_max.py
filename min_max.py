85f5946bdfe31a99b9460f4da225d9617f45055f
JTBOMAN <61206199+JTBOMAN@users.noreply.github.com>
2020-04-15 13:23:23 -0400

    Updated Author/Date Fixed print statements

--- min_max.py
+++ min_max.py
@@ -1,5 +1,5 @@
-# Josiah Boman
-# 04/14/2020
+# Author: Josiah Boman
+# Date: 04/14/2020
 # This program asks the user how many integers they would like to enter then prompt the user to enter
 # that many integers. After the numbers have been entered, the program displays the largest and smallest
 # of the numbers entered
@@ -20,7 +20,5 @@ while counter < integer_count:
     if num_1 > max:
         max = num_1
     counter = counter + 1
-print("Min is equal to " + str(min))
-print("Max is euqal to " + str(max))
-
-
+print("min: " + str(min))
+print("max: " + str(max))
